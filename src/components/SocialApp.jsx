d7edc9ac5a5a1cfc259c37b1b934e6e8c6229140
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-01-20 16:09:25 +0530

    (feat):add the NavBar component

--- src/components/SocialApp.jsx
+++ src/components/SocialApp.jsx
@@ -1,8 +1,10 @@
 import PostCard from "./PostCard"
+import NavBar from "./NavBar/NavBar"
 
 export default function SocialApp() {
   return (
     <div>
+        <NavBar/>
         <PostCard/>      
     </div>
   )
