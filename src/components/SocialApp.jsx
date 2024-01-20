d2ef83744d2393a30d2f9ac64e6191e2d8872d43
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-01-20 17:51:04 +0530

    (feat):add main container

--- src/components/SocialApp.jsx
+++ src/components/SocialApp.jsx
@@ -1,11 +1,12 @@
 import PostCard from "./PostCard"
 import NavBar from "./NavBar/NavBar"
+import MainContainer from "./MainContainer/MainContainer"
 
 export default function SocialApp() {
   return (
     <div>
         <NavBar/>
-        <PostCard/>      
+        <MainContainer/>
     </div>
   )
 }
